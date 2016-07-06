198e81ba6a5c367358a37dd52381951e3a34b8d4
TestSmirk <d.kc@qq.com>
2016-07-06 15:03:46 +0800

    init

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Learn_iOS
+//
+//  Created by BZBY on 16/7/6.
+//  Copyright © 2016年 BZBY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Learn_iOS
+//
+//  Created by BZBY on 16/7/6.
+//  Copyright © 2016年 BZBY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
